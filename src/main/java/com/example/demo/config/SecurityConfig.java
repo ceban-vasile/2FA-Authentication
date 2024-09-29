4b837bbff96fb738de81c96697111e8513416d25
Uber Mahomedov <53612811+huntrese@users.noreply.github.com>
2024-09-29 08:08:28 +0300

    упдате спрингбут

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/2fa/**").permitAll()
+                        .requestMatchers("/api/v1/thef/**").permitAll() // Added this line
                         .anyRequest().authenticated()
                 );
         return http.build();
@@ -44,7 +45,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
         MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
-        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.requestMatchers(mvcMatcherBuilder.pattern("/**")).permitAll());
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry
+                        .requestMatchers(mvcMatcherBuilder.pattern("/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/v1/thef/**")).permitAll() // Added this line
+        );
         return http.build();
     }
 }
